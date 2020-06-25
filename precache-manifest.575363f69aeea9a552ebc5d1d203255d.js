b528508cea76dd89340e159b9f61df54d09b0b0d
ZED-Magdy <zedmagdy@gmail.com>
2020-06-25 07:44:50 +0200

    fix vue router lazy routes

---
+++ precache-manifest.575363f69aeea9a552ebc5d1d203255d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e588b06bbec501ee20fce17a6962127",
+    "url": "./index.html"
+  },
+  {
+    "revision": "daf4e8f4cd73eb46f2ec",
+    "url": "./js/app.b94b715f.js"
+  },
+  {
+    "revision": "595345c2ac72d5484300",
+    "url": "./js/chunk-vendors.ba2f335f.js"
+  },
+  {
+    "revision": "12a591069b740fcc750eb306698fd57b",
+    "url": "./manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "./robots.txt"
+  }
+]);
