9b43f14b2c168ac8fcf6db3a334030ccfc8c7a2d
ZED-Magdy <zedmagdy@gmail.com>
2020-06-25 06:21:07 +0200

    fix service worker and scripts 1

--- dist/precache-manifest.fafa81f1c29726df19a3b49c2d1dd666.js
+++ dist/precache-manifest.fafa81f1c29726df19a3b49c2d1dd666.js
@@ -1,34 +1,34 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "4f40ad4de41196d5dd465f84d96607a4",
-    "url": "./index.html"
+    "url": "/index.html"
   },
   {
     "revision": "631f09d57843aeadef3b",
-    "url": "./js/app.87380176.js"
+    "url": "/js/app.87380176.js"
   },
   {
     "revision": "14fa54c189be3f6b720b",
-    "url": "./js/category.bddb67f4.js"
+    "url": "/js/category.bddb67f4.js"
   },
   {
     "revision": "fa286f83f687c3a9b39b",
-    "url": "./js/chunk-vendors.5aa491e4.js"
+    "url": "/js/chunk-vendors.5aa491e4.js"
   },
   {
     "revision": "47a6283eb3db05feccee",
-    "url": "./js/search.7169a4b7.js"
+    "url": "/js/search.7169a4b7.js"
   },
   {
     "revision": "e4111d663943739a6394",
-    "url": "./js/showpost.55680bf5.js"
+    "url": "/js/showpost.55680bf5.js"
   },
   {
     "revision": "12a591069b740fcc750eb306698fd57b",
-    "url": "./manifest.json"
+    "url": "/manifest.json"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "./robots.txt"
+    "url": "/robots.txt"
   }
 ]);
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.fafa81f1c29726df19a3b49c2d1dd666.js"
+  "/precache-manifest.fafa81f1c29726df19a3b49c2d1dd666.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "blogger"});
